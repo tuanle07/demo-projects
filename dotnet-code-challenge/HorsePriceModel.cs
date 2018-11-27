e81b89179e8d521239a376e15b09ad9d69f278ae
Le Tuan Anh <tuananh-le@outlook.com>
2018-11-28 03:44:50 +1100

    Add project files.

---
+++ dotnet-code-challenge/HorsePriceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_code_challenge
+{
+    public class HorsePriceModel
+    {
+        public string HorseName;
+        public double Price;
+    }
+}
---
+++ dotnet-code-challenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace dotnet_code_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            GetHorsePrice();
+        }
+
+        private static void GetHorsePrice()
+        {
+            XDocument doc = XDocument.Load(Directory.GetCurrentDirectory() + @".\FeedData\Caulfield_Race1.xml");
+            var testtemp = doc.Descendants("horse");
+            var test = from horse in doc.Descendants("horse")
+                       select new
+                       {
+                           HorseName = (string)horse.Attribute("name"),
+                           HorseNumber = (string)horse.Element("number")
+                       };
+            Console.WriteLine(test);
+        }
+    }
+}
